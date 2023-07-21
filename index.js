ec8c8d26e8422dddf7a5acee61a13c37b0c815ce
ishika prasad <dp878917@gmail.com>
2023-07-21 19:52:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var cel=document.getElementById("cel");
+var feh=document.getElementById("feh");
+cel.addEventListener('input',function(){
+    let c=this.value;
+    let f=(c*9/5)+32;
+    if(!Number.isInteger(f)){
+        f=f.toFixed(4);
+    }
+    feh.value = f;
+
+});
+feh.addEventListener('input',function(){
+    console.log("feh changed");
+    let f=this.value;
+    let c=(f-32)*5/9;
+    if(!Number.isInteger(c)){
+        c=c.toFixed(4);
+    }
+    cel.value=c;
+
+});
